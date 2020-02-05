fd4098aa5764b5c2b5b9b95d4c128e517d45416b
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-05 17:56:52 -0500

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(2, quote, strlen(quote));
+	return (1);
+}
