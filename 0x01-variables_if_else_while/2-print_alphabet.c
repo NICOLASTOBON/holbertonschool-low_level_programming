d877614504a22851c81cdce08ede1cf50c987682
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-06 11:56:53 -0500

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry poits
+ * putchar - alphabet
+ * Return - Success
+ */
+int main(void)
+{
+	char al = 'a';
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	return (0);
+}
