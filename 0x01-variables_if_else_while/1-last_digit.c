116803a70b87616a7d869774a1758ef6bb5eea3a
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-06 13:16:19 -0500

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there
+/**
+ * main - Entry point
  *
- * betty style doc for function main goes there
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
