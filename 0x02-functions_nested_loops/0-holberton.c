986a7441b264e72257b3dea97116967b66e8b408
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-10 14:44:14 -0500

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char holberton[] = "holberton";
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(holberton[i]);
