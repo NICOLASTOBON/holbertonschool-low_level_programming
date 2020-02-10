43c33789cc17e77ef20df2b241066a4f4a36b636
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-10 14:42:08 -0500

    0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+int main(void)
+{
+	char holberton[] = "holberton";
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(holberton[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
