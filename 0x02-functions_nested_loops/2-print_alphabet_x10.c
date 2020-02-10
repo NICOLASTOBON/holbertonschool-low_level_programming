9485b4599e01c5d283a3448c59f2b8a13aefeaa2
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-10 16:07:16 -0500

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+}
