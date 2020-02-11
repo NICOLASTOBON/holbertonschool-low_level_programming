637056b6993b910433791ab45911ceb47a0a71bb
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-11 16:28:58 -0500

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ *
+ * Return: Alaways 0 (Success)
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d \n", sum);
+	return (0);
+}
