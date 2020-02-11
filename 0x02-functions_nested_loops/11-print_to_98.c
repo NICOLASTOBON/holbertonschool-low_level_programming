cbd1cc51368f7fe4d304e0b9a0086e4b7e5def6d
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-11 11:27:04 -0500

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - numbers
+ *
+ * Return:  (0) Successs
+ */
+void print_to_98(int n)
+{
+
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d", n);
+			n++;
+			putchar(',');
+			putchar(' ');
+		}
+		else if (n > 98)
+		{
+			printf("%d", n);
+			n--;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	printf("%d", n);
+	putchar('\n');
+}
