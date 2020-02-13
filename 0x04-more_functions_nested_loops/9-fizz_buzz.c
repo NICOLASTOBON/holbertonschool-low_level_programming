e3c3e04c0a1e7c6d6939a935a6064fbdb158be8c
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-13 16:04:35 -0500

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,25 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 5 == 0) && (i % 3 == 0))
+		{
 			printf("FizzBuzz ");
+		}
 		else if ((i % 5 == 0) && (!(i % 3 == 0)))
-			printf("Buzz ");
+		{
+			printf("Buzz");
+
+			if (i != 100)
+				putchar(' ');
+		}
 		else if ((i % 3 == 0) && (!(i % 5 == 0)))
+		{
 			printf("Fizz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
+
 	}
-	putchar('\n');
+	printf("\n");
 }
