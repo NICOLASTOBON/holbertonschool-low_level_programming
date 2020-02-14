fe53817c7e2e7a2d5f7026eccc2c575846e3bd09
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-13 20:11:43 -0500

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 2;
+	long int b = 612852475143;
+
+	while (b != a)
+	{
+		if (b % a == 0)
+			b = b / a;
+		else
+			a++;
+	}
+	printf("%d \n", a);
+}
