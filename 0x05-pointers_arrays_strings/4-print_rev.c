e1f26036c68054bffa52e1f1f93ce71150ed0465
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-17 12:32:16 -0500

    header file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_rev - function that prints a string.
+ * @s: is a parameter
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (j = i; j >= 0; j--)
+		_putchar(s[j]);
+
+	_putchar('\n');
+}
