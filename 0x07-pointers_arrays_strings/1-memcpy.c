1066cc3d77f986c0d3b305fe252aeb8ac2760319
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-24 11:09:38 -0500

    function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: is a parameter
+ * @src: is a parameter
+ * @n: is a parameter
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
