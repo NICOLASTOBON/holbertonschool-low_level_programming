3e97e09c12165597fc8101faa8d4bd739d4ce0e1
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-29 18:22:16 -0500

    program that prints its name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry points
+ * @argc: is a parameter
+ * @argv: is a parameter
+ * Return: Always (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
