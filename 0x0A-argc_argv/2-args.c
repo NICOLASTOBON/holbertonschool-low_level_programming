eeb50b75bb59d4dc28c0e362e80f1f14308b74b8
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-02-29 18:31:16 -0500

    program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry points
+ * @argc: is a parameter
+ * @argv: is a parameter
+ * Return: Always (0) Success
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+
+	for (i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+
+	return (0);
+}
