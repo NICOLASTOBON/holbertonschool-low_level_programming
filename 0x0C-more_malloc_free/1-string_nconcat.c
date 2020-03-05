3e267599e1ac943fd9c68daed85dca1898c5dddc
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-05 15:17:22 -0500

    function that concat a string.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,9 +26,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	lon1 = _strlen(s1);
 	lon2 = _strlen(s2);
