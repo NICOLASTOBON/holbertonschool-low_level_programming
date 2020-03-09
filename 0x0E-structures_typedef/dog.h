bf1d7903aeb757c9f8eb54a209543cf593186ffd
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-09 15:59:38 -0500

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - is a struct
+ * @name: is a parameter
+ * @age: is a parameter
+ * @owner: is a parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
