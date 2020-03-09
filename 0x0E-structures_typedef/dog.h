c66d14635a9aa7fcae248b385a3c033048a703ed
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-09 17:02:59 -0500

    header files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - is a struct
  * @name: is a parameter
@@ -12,5 +13,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
