e29de619a545f828115314df26939688fd57d2f2
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-11 18:13:17 -0500

    Entry points

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,9 +19,9 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 6)
+	while (i < 5)
 	{
-		if (*ops[i].op == *s)
+		if (*ops[i].op == s[0] && s[1] == '\0')
 		{
 			return (ops[i].f);
 		}
