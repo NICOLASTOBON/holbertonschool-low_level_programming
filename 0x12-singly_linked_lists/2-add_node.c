f79b9d7dc55b0212f261a1ef27149c913822bd9d
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-19 16:17:10 -0500

    function that adds a new node at the beginning of a list_t list.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * _strlen - return length of string
+ * @str: is a parameter
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+/**
+ * createNodo - nodo created
+ * @str: is a parameter
+ * @len: is a length
+ * Return: new nodo
+ */
+list_t *createNodo(const char *str, unsigned int len)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	new->str = strdup(str);
+	new->len = len;
+	return (new);
+}
+/**
+ * add_node - function that adds a new node at the beginning of a list_t
+ * @head: is a parameter
+ * @str: is a parameter
+ * Return: sucess
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = createNodo(str, strlen(str));
+
+	new->next = *head;
+	*head = new;
+
+	return (0);
+}
