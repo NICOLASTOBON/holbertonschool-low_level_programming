2d25cec8459f4e17fa4292ea1e4ed8c7bf952564
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-29 16:54:27 -0500

    fuck putchar

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,13 @@
  */
 int main(void)
 {
-	int i, sum;
+	int i, sum = 0;
 
 	for (i = 1; i <= 1024; i++)
 	{
 		if (i % 5 == 0 || i % 3 == 0)
-		{
-			sum = sum + i;
-		}
+			sum += i;
 	}
-	printf("%d \n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
