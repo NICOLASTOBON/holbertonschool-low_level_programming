4c79a33391e1a5470f5d68f28a38c747183d6d6a
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-31 17:21:52 -0500

    reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: is the source file
+ * @letters: is the number of character
+ * Return:  number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wd;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	rd = read(fd, buff, letters);
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1 || wd != rd)
+		return (0);
+
+	free(buff);
+	close(fd);
+	return (wd);
+}
