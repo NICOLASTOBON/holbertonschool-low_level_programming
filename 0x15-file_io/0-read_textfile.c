bd61d47485fcd7c5abd0f6e9e8a90e09f95f807d
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-01 10:11:27 -0500

    text files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,27 +12,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
+
 	fd = open(filename, O_RDWR);
 	if (fd == -1 )
 	{
 		free(buff);
 		return (0);
 	}
+
 	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
 		free(buff);
 		return (0);
 	}
+
 	wd = write(STDOUT_FILENO, buff, rd);
 	if (wd == -1 || wd != rd)
 	{
 		free(buff);
 		return (0);
 	}
+
 	free(buff);
 	close(fd);
 	return (wd);
