03f90a0cce46b42c1ca29749fc53bc17ed9cead2
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-31 19:08:26 -0500

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		wd = write(STDOUT_FILENO, text_content, _strlen(text_content));
+		wd = write(fd, text_content, _strlen(text_content));
 		if (wd == -1)
 			return (-1);
 	}
