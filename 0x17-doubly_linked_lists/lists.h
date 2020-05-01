7b29e6b9aaf8888f7304ee1a454375ca4ead9c9c
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-01 16:54:27 -0500

    add files

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -12,9 +14,12 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+#endif
