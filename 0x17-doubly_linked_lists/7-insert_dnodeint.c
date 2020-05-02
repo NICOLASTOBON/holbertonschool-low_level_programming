a69f80ff038f95bec130143d9da74464893d582b
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-02 13:20:07 -0500

    inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,8 +18,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	if (h == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = n;
-	if (idx == 0 || (*h == NULL))
+	if (idx == 0)
 	{
 		free(new);
 		return (add_dnodeint(h, n));
