46b2e6623a5109cbc8d0f93842dbd6c1f722b008
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-29 13:04:14 -0500

    add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,9 +14,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || strcmp(key, "") == 0 || key == NULL || value == NULL)
 		return (0);
 
-
 	idx = key_index((unsigned char *)key, ht->size);
 	current = ht->array[idx];
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (new->key == NULL || new->value == NULL)
+	{
+		free(new);
+		return (0);
+	}
 	if (current != NULL)
 	{
 		while (current != NULL)
@@ -24,24 +34,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (strcmp(key, current->key) == 0)
 			{
 				free(current->value);
-				current->value = strdup(value);
+				current->value = strdup((char *)value);
 				return (1);
 			}
 		}
 	}
 
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-	new->key = strdup(key);
-	new->value = strdup(value);
-
-	if (new->key == NULL || new->value == NULL)
-	{
-		free(new);
-		return (0);
-	}
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
