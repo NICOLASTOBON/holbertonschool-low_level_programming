5a46fe660e9e2a226db0653c68862fbf1e2a5f53
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-29 14:02:06 -0500

    delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,24 +6,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *current, *tmp;
-    unsigned long int i;
+	hash_node_t *current, *tmp;
+	unsigned long int i;
 
-    if (ht != NULL)
-    {
-        for (i = 0; i < ht->size; i++)
-        {
-            current = ht->array[i];
-            while (current != NULL)
-            {
-                tmp = current;
-                free(tmp->key);
-                free(tmp->value);
-                free(tmp);
-                current = current->next;
-            }
-        }
-        free(ht->array);
-        free(ht);
-    }
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				tmp = current;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				current = current->next;
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
 }
