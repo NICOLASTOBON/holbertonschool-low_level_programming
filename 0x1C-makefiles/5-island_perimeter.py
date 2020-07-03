8c1c9774a60d01c3fa6eaf4c4320a16242219b8f
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-07-03 18:28:44 -0500

    [add] perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,8 +7,10 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                value += 1
-                if grid[i - 1][j] == 1 and grid[i][j + 1] == 1:
-                    value += 1
-
-    return int(value * 4 / 2)
+                value += 2
+                if grid[i - 1][j] == 1:
+                    value -= 1
+                if grid[i][j - 1] == 1:
+                    value -= 1
+  
+    return value * 2
