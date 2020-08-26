6cacdbb2d68815277ff7533cbc1872a3e1531271
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-26 02:57:05 -0500

    [add]  searches for a value in a sorted array of integers

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+/**
+ * print_array - print element to the array
+ * @array: is a pointer to the first element to the array
+ * @start: number of element in array
+ * @end: number of element in array
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	unsigned int i;
+
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("%i\n", array[i]);
+}
+/**
+ * bn_search - searching
+ * @array: is a pointer to the first element to the array
+ * @left: number of element in array
+ * @right: number of element in array
+ * @value: is the valor to search
+ * Return: the index
+ */
+int bn_search(int *array, size_t left, size_t right, int value)
+{
+	if (left <= right)
+	{
+		int mid = (left + right) / 2;
+
+	    print_array(array, left, right);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			return (bn_search(array, left, mid - 1, value));
+		else
+			return (bn_search(array, mid + 1, right, value));
+	}
+	return (-1);
+}
+/**
+ * binary_search - searching
+ * @array: is a pointer to the first element to the array
+ * @size: number of elements in array
+ * @value: is the valor to search
+ * Return: the index
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int num;
+
+	num = bn_search(array, 0, size - 1, value);
+	return (num);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+int bn_search(int *array, size_t left, size_t right, int value);
+void print_array(int *array, size_t start, size_t end);
 
 #endif
