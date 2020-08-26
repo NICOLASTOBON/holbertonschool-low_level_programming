a08009cc8a849abef989ef05b213d63cb576f199
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-26 12:37:17 -0500

    searching

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int bn_search(int *array, size_t left, size_t right, int value);
 void print_array(int *array, size_t start, size_t end);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
